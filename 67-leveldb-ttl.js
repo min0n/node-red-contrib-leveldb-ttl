0bfb77378e259e0e81627cfa47fbd3e1212d9011
pi <fernandogalin@gmail.com>
2015-10-06 20:21:22 +0200

    initial

---
+++ 67-leveldb-ttl.js
@@ -0,0 +1,275 @@
+/**
+ * Copyright 2013,2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * 06 Oct - 2015 added TTL, Readers and Batch support. by min0n
+ *
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+    var lvldb = require('level'),
+        lvlttl = require('level-ttl');
+
+    function LevelNode(n) {
+        RED.nodes.createNode(this,n);
+        this.dbname = n.db;
+        this.defaultttl = n.defaultttl;
+        this.checkfreq = n.checkfreq;
+        this.ready = false;
+        var node = this;
+        lvldb(this.dbname, function(err, db) {
+            if (err) { node.error(err); }
+            //console.log('default ttl (s)='+node.defaultttl+' checkfreq (s)='+node.checkfreq);
+            node.db = lvlttl(db,{defaultTTL: 1000*node.defaultttl, checkFrequency: 1000*node.checkfreq }); //s to msec
+            node.db.on('ready', function() { node.ready = true; });
+            node.db.on('closing', function() { node.ready = false; });
+        });
+        node.on('close', function() {
+            if (node.db) {
+                node.ready = false;
+                node.db.close();
+            }
+        });
+    }
+    RED.nodes.registerType("leveldbase",LevelNode);
+
+
+    function LevelDBNodeIn(n) {
+        RED.nodes.createNode(this,n);
+        this.level = n.level;
+        this.levelConfig = RED.nodes.getNode(this.level);
+
+        var node = this;
+        node.on("input", function(msg) {
+            if (node.levelConfig && node.levelConfig.ready) {
+                var key = msg.topic.toString();
+                if (key && (key.length > 0)) {
+                    node.levelConfig.db.get(msg.topic, function(err, value) {
+                        if (err) {
+                            //node.warn(err);
+                            // for some reason they treat nothing found as an error...
+                            msg.payload = null;  // so we should return null
+                        }
+                        else { msg.payload = value; }
+                        node.send(msg);
+                    });
+                }
+                else { node.error("Cannot make key string from msg.topic"); }
+            }
+            else { node.error("Database not ready",msg); }
+        });
+    }
+    RED.nodes.registerType("leveldb in",LevelDBNodeIn);
+
+
+    function LevelDBNodeOut(n) {
+        RED.nodes.createNode(this,n);
+        this.level = n.level;
+        this.operation = n.operation;
+        this.nodettl = n.nodettl;
+        this.levelConfig = RED.nodes.getNode(this.level);
+
+        var node = this;
+        console.log('nodettl dice '+node.nodettl+' default ttl dice '+node.levelConfig.defaultttl);
+        node.on("input", function(msg) {
+            if (node.levelConfig && node.levelConfig.ready) {
+                var key = msg.topic.toString();
+                if (key && (key.length > 0)) {
+                    if (node.operation === "delete") {
+                        node.levelConfig.db.del(msg.topic);
+                    }
+                    else {
+                        
+                        var ttl = (msg.hasOwnProperty('ttl') && msg.ttl>0) ? msg.ttl : node.nodettl; //msg ttl overrides defined node ttl
+                        
+                        if (ttl > 0) //override default ttl?
+                        {
+                            node.levelConfig.db.put(msg.topic, msg.payload,{ttl: ttl*1000 },function(err) {
+                                if (err) { node.error(err); }
+                            });
+                            
+                        }else //not ttl or default ttl.
+                        {
+                            node.levelConfig.db.put(msg.topic, msg.payload, function(err) {
+                                if (err) { node.error(err); }
+                            });
+                        }
+                        
+                        
+                    }
+                }
+                else { node.error("Cannot make key string from msg.topic"); }
+            }
+            else { node.error("Database not ready",msg); }
+        });
+    }
+    RED.nodes.registerType("leveldb out",LevelDBNodeOut);
+    
+    
+    
+    //06 oct 2015. new nodes
+    
+    function LevelDBNodeStream(n) {
+        RED.nodes.createNode(this,n);
+        this.level = n.level;
+        this.stype = n.stype;
+        this.levelConfig = RED.nodes.getNode(this.level);
+
+        var node = this;
+        node.on("input", function(msg) {
+            if (node.levelConfig && node.levelConfig.ready) {
+                var options = msg.payload || {};
+                
+                var values = (node.stype != "key");
+                var keys = (node.stype != "value");
+                
+                //override node stream type 
+                options.values = options.hasOwnProperty('values') ? options.values : values ;
+                options.keys = options.hasOwnProperty('keys') ? options.keys : keys ;
+                    
+                if (options.values && options.keys)//(node.stype == "read")
+                {
+                    var readStream =  node.levelConfig.db.createReadStream(options);
+                    readStream.on('data',function (data) {
+                        msg.topic = data.key;
+                        msg.payload = data.value;
+                        node.send(msg);
+                    });
+                }else if (options.keys)
+                {
+                    var keyStream =  node.levelConfig.db.createKeyStream(options);
+                    keyStream.on('data',function (data) {
+                        //msg.topic = data.key;
+                        msg.payload = data.key;
+                        node.send(msg);
+                    });
+                }else //if (node.stype == "value")
+                {
+                    var valueStream =  node.levelConfig.db.createValueStream(options);
+                    valueStream.on('data',function (data) {
+                        //msg.topic = data.key;
+                        msg.payload = data.value;
+                        node.send(msg);
+                    });
+                
+                }
+            }
+            else { node.error("Database not ready",msg); }
+        });
+    }
+    RED.nodes.registerType("leveldb stream",LevelDBNodeStream);
+
+    function LevelDBNodeKeyTTL(n) {
+        RED.nodes.createNode(this,n);
+        this.level = n.level;
+        this.nodettl = n.nodettl;
+        this.levelConfig = RED.nodes.getNode(this.level);
+
+        var node = this;
+        console.log('LevelDBNodeKeyTTL:: nodettl dice '+node.nodettl+' default ttl dice '+node.levelConfig.defaultttl);
+        node.on("input", function(msg) {
+            if (node.levelConfig && node.levelConfig.ready) {
+                var key = msg.topic.toString();
+                if (key && (key.length > 0)) {
+                    var ttl = (msg.hasOwnProperty('ttl') && msg.ttl>0) ? msg.ttl : node.nodettl; //msg ttl overrides defined node ttl
+                    if (ttl > 0) //override default ttl?
+                    {
+                        console.log('override default ttl '+ttl);
+                        node.levelConfig.db.ttl(msg.topic,{ttl: ttl*1000 },function(err) {
+                            if (err) { node.error(err); }
+                        });        
+                    }else //set default ttl if any
+                    {
+                        node.levelConfig.db.ttl(msg.topic,{ttl: node.levelConfig.defaultttl*1000 },function(err) {
+                            if (err) { node.error(err); }
+                        });        
+                    }
+                }
+                else { node.error("Cannot make key string from msg.topic"); }
+            }
+            else { node.error("Database not ready",msg); }
+        });
+    }
+    RED.nodes.registerType("leveldb keyttl",LevelDBNodeKeyTTL);
+
+    
+     function LevelDBNodeBatchOut(n) {
+        RED.nodes.createNode(this,n);
+        this.level = n.level;
+        this.nodettl = n.nodettl;
+        this.levelConfig = RED.nodes.getNode(this.level);
+
+        var node = this;
+        console.log('LevelDBNodeBatchOut:: nodettl dice '+node.nodettl+' default ttl dice '+node.levelConfig.defaultttl);
+        node.on("input", function(msg) {
+            if (node.levelConfig && node.levelConfig.ready) {
+                var ttl = (msg.hasOwnProperty('ttl') && msg.ttl>0) ? msg.ttl : node.nodettl; //msg ttl overrides defined node ttl
+                if (ttl>0) //override default ttl?
+                {
+                    node.levelConfig.db.batch(msg.payload,{ttl: ttl*1000 },function(err) {
+                        if (err) { node.error(err); }
+                    });   
+                }else //set default ttl if any
+                {
+                     node.levelConfig.db.batch(msg.payload,{ttl: node.levelConfig.defaultttl*1000 },function(err) {
+                         if (err) { node.error(err); }
+                     });        
+                }
+            }
+            else { node.error("Database not ready",msg); }
+        });
+    }
+    RED.nodes.registerType("leveldb batchout",LevelDBNodeBatchOut);
+    
+    function LevelDBNodeBatchOut2(n) {
+        RED.nodes.createNode(this,n);
+        this.level = n.level;
+        this.nodettl = n.nodettl;
+        this.batchsize = n.batchsize;
+        this.buffer = [];
+        this.levelConfig = RED.nodes.getNode(this.level);
+
+        var node = this;
+        
+        
+        console.log('LevelDBNodeBatchOut2:: nodettl dice '+node.nodettl+' default ttl dice '+node.levelConfig.defaultttl);
+        node.on("input", function(msg) {
+            if (node.levelConfig && node.levelConfig.ready) {
+                node.buffer.push(msg.payload);
+                console.log("LevelDBNodeBatchOut2:: array "+node.buffer.length);
+                
+               if (node.buffer.length > 0) {
+                     node.status({text:node.buffer.length});
+                }
+
+                if (node.buffer.length >= node.batchsize) //do batch
+                {
+                    console.log("yupi doing batch!!!");
+                    var ttl = (node.nodettl) > 0 ? node.nodettl : node.levelConfig.defaultttl; //msg ttl overrides defined node ttl
+                    
+                    node.levelConfig.db.batch(node.buffer,{ttl: ttl*1000 },function(err) {
+                        if (err) { node.error(err); }
+                    });        
+                    
+                    node.buffer = []; //reset array
+                }
+            }
+            else { node.error("Database not ready",msg); }
+        });
+    }
+    RED.nodes.registerType("leveldb batchout2",LevelDBNodeBatchOut2);
+    
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# node-red-contrib-leveldb-ttl
-adds level-ttl, streams, and batch support to the node-red-node-leveldb
+node-red-node-leveldb
+=====================
+A <a href="http://nodered.org" target="_new">Node-RED</a> node to read and write to a LevelDB database.
+
+Install
+-------
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install node-red-node-leveldb
+
+
+Usage
+-----
+
+Allows basic access to a LevelDB database.
+
+Uses <a href="https://code.google.com/p/leveldb/" target="_new"><i>LevelDB</i></a> for a simple key value pair database.
+
+There are two node to choose from...
+
+Use one node to either <b>put</b> (store) the <b>msg.payload</b> to the named database file, using <b>msg.topic</b> as the key, or to <b>delete</b> information select delete in the properties dialogue and again use <b>msg.topic</b> as the key.</b>.
+
+Use the other node to <b>get</b>, or retrieve the data already saved in the database.
+
+Again use <b>msg.topic</b> to hold the <i>key</i> for the database, and the result is returned in <b>msg.payload</b>. If nothing is found for the key then <i>null</i> is returned.
+
+---
+
+06 Oct 2015 node-red-contrib-leveldb-ttl
+
+This is a quick and dirty package to add <a href="https://github.com/Level/level-ttl" target="_new"><i>Level-ttl</i></a> support to the original <a href="https://github.com/node-red/node-red-nodes/tree/master/storage/leveldb" target="_new"><i>node-red-node-leveldb</i></a> code (version 0.0.5), and to wrap some <a href="https://github.com/Level/levelup" target="_new"><i>Levelup</i></a> functions, like batch and streams.
+
+The configuration node allows to set the database <b>Default TTL</b>in seconds (0: disabled), and <b>Scan TTL</b> to set the internal scan in seconds. The <b>Default TTL</b> can be overriden.
+
+The node to <b>get</b> from the database works as the original. 
+
+Some info about the new nodes:
+
+<b>Stream</b> node: The configuration of this node defines the default <b>type of stream</b>: ReadStream, KeyStream or ValueStream. This can be overriden using <b>msg.payload.keys</b> and <b>msg.payload.values</b>. The range of keys that are streamed can be set by using <b>msg.payload.start</b>, <b>msg.payload.end</b>, <b>msg.payload.lt</b>, <b>msg.payload.gt</b> or <b>msg.payload.limit</b>.  If no options are passed, it streams the entire database.
+Example:
+msg.payload = {
+    keys: true,
+    values: false,
+    start: 'key1~',
+    end: 'key1~~'
+}
+
+<b>Batch</b> node: can be used for very fast bulk-write operations (both put and delete). <b>msg.payload</b> should contain a list of operations to be executed sequentially, although as a whole they are performed as an atomic operation inside LevelDB. 
+Example:
+msg.payload =[
+                {type: 'put', key: 'key1', value: 123},
+                {type: 'del', key: 'key2'}
+            ]
+
+
+<b>Batch2</b> node: This node get individuals operations and push them in a buffer before doing the batch operation to the database. The configuration of this node allows to set the buffer size. 
+
+<b>KeyTTL</b> node: This node can serve to insert or update a ttl for any given key from <b>msg.topic</b> in the database (even if that key doesn't exist but may in the future). 
+
+
+The configuration of the nodes to <b>put</b>, to <b>batch</b> and the <b>keyttl</b> node, include a <b>Node TTL</b> parameter to override the <b>Default TTL</b>. Also, <b>Node TTL</b> can be individually overriden by a <b>msg.ttl</b>.
+
+
+
+
